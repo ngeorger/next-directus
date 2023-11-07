276d053f9b86f493541e2c9a1fefd8c7b56a1096
Nicolás Georger <info@sredevops.cl>
2023-11-07 14:11:51 -0300

    Update embed.ts
    Signed-off-by: Nicolás Georger <info@sredevops.cl>

--- lib/utils/embed.ts
+++ lib/utils/embed.ts
@@ -1,13 +1,5 @@
 //Generate the Youtube, Vimeo, or Loom embed URL based on an URL from the user
 export function generateVideoEmbed(url: string) {
-  if (url.includes('https://youtube.com/')) {
-    return `https://www.youtube.com/embed/${url.split('v=')[1]}`
-  }
-  // Handle youtu.be links
-  else if (url.includes('https://youtu.be/')) {
-    return `https://www.youtube.com/embed/${url.split('be/')[1]}`
-  } else {
     return url
-  }
 }
 
